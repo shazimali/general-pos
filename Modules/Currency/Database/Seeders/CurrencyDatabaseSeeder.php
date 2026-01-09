4943e0861474567e759dbbba4b61877231e2a794
shazim ali <shazimali03@gmail.com>
2026-01-09 15:58:27 +0500

    currency change

--- Modules/Currency/Database/Seeders/CurrencyDatabaseSeeder.php
+++ Modules/Currency/Database/Seeders/CurrencyDatabaseSeeder.php
@@ -15,11 +15,12 @@ class CurrencyDatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run() {
+    public function run()
+    {
         Currency::create([
-            'currency_name'      => 'US Dollar',
-            'code'               => Str::upper('USD'),
-            'symbol'             => '$',
+            'currency_name'      => 'PAK Rupee',
+            'code'               => Str::upper('PKR'),
+            'symbol'             => 'PKR',
             'thousand_separator' => ',',
             'decimal_separator'  => '.',
             'exchange_rate'      => null
